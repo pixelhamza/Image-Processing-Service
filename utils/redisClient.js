7f49836ebea5f2602d78c977821b66b33eab45a0
pixelhamza <nashithwk25@gmail.com>
2026-01-24 17:44:59 +0530

    setup redis

---
+++ utils/redisClient.js
@@ -0,0 +1,16 @@
+const {createClient}=require("redis"); 
+
+
+const redis=createClient({ 
+    url:process.env.REDIS_URL
+});
+
+redis.on("error",(err)=>{ 
+    console.error("Redis error",err);
+})
+
+(async()=>{ 
+    await redis.connect();
+})();
+
+module.exports=redis;
